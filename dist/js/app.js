45bc268aa0c28313608d25863ad54f0b5c2edf42
Geoff Lewis <geoff@geofflewis.work>
2018-02-26 14:03:12 -0700

    Added JavaScript & jQuery to the banner

---
+++ dist/js/app.js
@@ -0,0 +1,23 @@
+// Variables
+const profilePic = document.querySelector('img');
+const heading = document.querySelector('h1');
+const subHeading = document.querySelector('figcaption');
+let i = 0;
+let firstText = "Hello, I'm Geoff!  Welcome to my online portfolio."
+let speed = 100;
+
+
+// Fade in the profile picture
+$(profilePic).hide().fadeIn(6000);
+
+
+//Type out the title and subtitle in the banner
+function typeHeading() {
+     if (i < firstText.length) {
+          heading.innerHTML += firstText.charAt(i);
+          i++;
+          setTimeout(typeHeading, speed);
+     }
+}
+
+typeHeading();
